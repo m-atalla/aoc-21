bcbfb84defa70b15cae430a04ab3b2e8315bbd8e
Mohammed Atallah <mo.atalla96@gmail.com>
2022-04-04 17:16:51 +0200

    add day4 part 2 solution

--- 04/bingo.hs
+++ 04/bingo.hs
@@ -13,7 +13,10 @@ main = do
         boardSrc = delete "" (tail input)
         boards = parseBoards boardSrc []
 
-    print $ solvePart1 $ bingo draw boards
+    putStrLn "Part 1:"
+    print $ solve $ bingo draw boards
+    putStrLn "Part 2:"
+    print $ solve $ bingoLast draw boards
     return ()
 
 parseBoards :: [String] -> [Board] -> [Board]
@@ -36,14 +39,22 @@ markBoard m = map $ map (`markCell` m)
 gameRound :: Int -> [Board] -> [Board]
 gameRound m = map (m `markBoard`)
 
-bingo :: [Int] -> [Board] -> ([Board], Int)
-bingo [] boards = (boards, 0)
-bingo (d:ds) boards = if winnerFound then (winningBoard, d) else bingo ds nextBoards
+bingo :: [Int] -> [Board] -> (Board, Int)
+bingo [] boards = error "No winning boards"
+bingo (d:ds) boards = if winnerFound then (head winningBoard, d) else bingo ds nextBoards
     where
         nextBoards = gameRound d boards
         winningBoard = filter isWinningBoard nextBoards
         winnerFound = length winningBoard == 1
 
+bingoLast :: [Int] -> [Board] -> (Board, Int)
+bingoLast [] boards = error "No winning boards"
+bingoLast (d:ds) boards = if lastTurn then (head winningBoard, d) else bingoLast ds newBoards
+    where
+        nextBoards = gameRound d boards
+        newBoards = filter (not . isWinningBoard) nextBoards
+        winningBoard = filter isWinningBoard nextBoards
+        lastTurn = null newBoards
 
 isWinningBoard :: Board -> Bool
 isWinningBoard board = hWin || vWin
@@ -54,9 +65,8 @@ isWinningBoard board = hWin || vWin
         vWin = any hasWinRow flippedBoard
 
 
--- incomplete pattern doesnt matter here
-solvePart1 :: ([Board], Int) -> Int
-solvePart1 ([b], w) = unmarkedSum * w
+solve :: (Board, Int) -> Int
+solve (b, w) = unmarkedSum * w
     where
         unmarkedSum = foldr (\(n, b) acc -> if not b then n + acc else acc) 0 $ concat b
 
