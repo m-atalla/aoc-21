56e7dbdd446a1ff80ebef63e2c63be55a8ad53e7
Mohammed Atallah <mo.atalla96@gmail.com>
2022-04-14 15:15:51 +0200

    add 05 part 2 solution

--- 05/colide.hs
+++ 05/colide.hs
@@ -15,19 +15,18 @@ main = do
     c <- readFile "./input.txt"
     let
         input = lines c
-        parsed = parseLine input
-        parsed2 = map flattenPointStr parsed
-        parsed3 = map convertToLine parsed2
-        vents = concatMap (\x -> genPoints (src x) (dest x)) $ filter isHorV parsed3
-        sol1 = part1 vents
+        coordLines = map (convertToLine . (flattenPointStr . removeArrows)) input
+        colide = concatMap (\x -> genPoints (src x) (dest x)) coordLines
+        colideFiltered = concatMap (\x -> genPoints (src x) (dest x)) $ filter isHorV coordLines
 
-    print sol1
+    putStr "Part 1: "
+    print $ solve colideFiltered
+    putStr "Part 2: "
+    print $ solve colide
     return ()
 
 toInt x = read x :: Int
 
-parseLine :: [String] -> [[String]]
-parseLine = map removeArrows
 
 -- Applies the following string transformation:
 -- "x1,y1 -> x2,y2" -> ["x1,y1","x2,y2"]
@@ -37,7 +36,10 @@ removeArrows x = words $ x \\ "->"
 -- Applies the following transformation:
 -- ["x1,y1","x2,y2"] -> [x1,y1,x2,y2]
 flattenPointStr :: [String] -> [String]
-flattenPointStr = concatMap (deleteByIdx 1 . groupBy ((==) `on` isDigit))
+flattenPointStr = concatMap removeComma
+            where
+                -- "x,y" -> ["x", "y"]
+                removeComma = deleteByIdx 1 . groupBy ((==) `on` isDigit)
 
 convertToLine :: [String] -> Line
 convertToLine l = Line { src = (x1 l, y1 l), dest = (x2 l,y2 l)}
@@ -53,15 +55,19 @@ deleteByIdx idx xs = before ++ after
 
 
 -- Generates intermediate points from source to destination
+genPoints :: Point -> Point -> [Point]
 genPoints (x1 , y1) (x2, y2)
-    | x1 == x2 && y1 < y2 = zip (repeat x1) [y1..y2]
-    | x1 == x2 && y1 > y2 = zip (repeat x1) [y2..y1]
-    | x1 > x2 && y1 == y2 = zip [x2..x1] (repeat y1)
-    | x1 < x2 && y1 == y2 = zip [x1..x2] (repeat y1)
+    | x1 == x2 = zip (repeat x1) [(min y1 y2)..(max y1 y2)]
+    | y1 == y2 = zip [(min x1 x2)..(max x1 x2)] (repeat y1)
+    | x1 < x2  && y1 < y2   = zip [x1..x2] [y1..y2]
+    | x1 < x2  && y1 > y2   = zip [x1..x2] $ reverse [y2..y1]
+    | x1 > x2  && y1 < y2   = zip [x2..x1] $ reverse [y1..y2]
+    | x1 > x2  && y1 > y2   = zip (reverse [x2..x1]) (reverse [y2..y1])
     | otherwise = [(x1,y1)] -- both xs and ys are equal, meaning it is only a single point
 
-part1 vs = length $ filter ((>= 2) . length) . groupBy (\(x1, y1) (x2, y2) -> x1 == x2 && y1 == y2 ) $ sort vs
+solve vs = length $ filter ((>= 2) . length) . groupBy (\(x1, y1) (x2, y2) -> x1 == x2 && y1 == y2 ) $ sort vs
 
+isHorV :: Line -> Bool
 isHorV line = x1 == x2 || y1 == y2
     where
         (x1, y1) = src line
