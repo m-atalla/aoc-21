554b048453658e00e92d7c36bd41fa27a0d68342
Mohammed Atallah <mo.atalla96@gmail.com>
2022-04-20 17:37:50 +0200

    add day 7 part 2

--- 07/crab.hs
+++ 07/crab.hs
@@ -8,9 +8,14 @@ main :: IO ()
 main = do
     file <- readFile "./input.txt"
     let
-        input = map toInt . filter (/= ","). init . groupBy ( (==) `on` isDigit ) $ file
+        testInput = [16,1,2,0,4,2,7,1,2,14]
+        input = map toInt . 
+                filter (/= ",") . 
+                init . 
+                groupBy ((==) `on` isDigit) $ file
 
-    print $ lowestCost input
+    putStrLn $ "Part 1: " ++ show (lowestCost  input)
+    putStrLn $ "Part 2: " ++ show (lowestCostSeries input)
 
 toInt x = read x :: Int
 
@@ -20,14 +25,24 @@ positionHistogram = sortBy (\(a, _) (b, _) -> compare b a) .
                     group .
                     sort
 
-
 alignCost :: Int -> Int -> Int
 alignCost to from = abs (to - from)
-
-
 
 lowestCost :: [Int] -> Int
 lowestCost iniPos = minimum allAlignments
     where
         allAlignments = map (sum . (\pos -> map (alignCost pos) iniPos)) likelyPos
         likelyPos = map snd . positionHistogram $ iniPos
+
+-- part 2
+alignCostSeries :: Int -> Int -> Int
+alignCostSeries to from = let
+                            n = alignCost to from 
+                          in
+                            n * (n + 1) `div` 2
+
+lowestCostSeries :: [Int] -> Int
+lowestCostSeries iniPos = minimum allAlignments
+    where
+        allAlignments = map (sum . (\pos -> map (alignCostSeries pos) iniPos)) likelyPos
+        likelyPos = [0 .. maximum iniPos]
